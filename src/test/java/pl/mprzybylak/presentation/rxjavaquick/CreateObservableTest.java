29b01c6b6b42ed2d27c2ac9a6e3effbf47ba26c0
Michał Przybylak <michal.przybylak@gmail.com>
2016-11-11 16:48:35 +0100

    fromCallable example

--- src/test/java/pl/mprzybylak/presentation/rxjavaquick/CreateObservableTest.java
+++ src/test/java/pl/mprzybylak/presentation/rxjavaquick/CreateObservableTest.java
@@ -6,6 +6,8 @@ import pl.mprzybylak.presentation.rxjavaquik.Money;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -53,6 +55,30 @@ public class CreateObservableTest {
 
         // then
         assertThat(totalExpenses).isEqualTo(money(170));
+    }
+
+    @Test
+    public void lazyMethodCall() {
+
+        // given
+        final AtomicInteger counter = new AtomicInteger(0);
+        Callable<Integer> tick = counter::incrementAndGet;
+
+        AtomicInteger firstTick = new AtomicInteger(0);
+        AtomicInteger secondTick = new AtomicInteger(0);
+        AtomicInteger thirdTick = new AtomicInteger(0);
+
+        // when
+        Observable<Integer> observable = Observable.fromCallable(tick);
+
+        observable.subscribe(firstTick::set);
+        observable.subscribe(secondTick::set);
+        observable.subscribe(thirdTick::set);
+
+        // then
+        assertThat(firstTick.get()).isEqualTo(1);
+        assertThat(secondTick.get()).isEqualTo(2);
+        assertThat(thirdTick.get()).isEqualTo(3);
     }
 
     private Money money(long value) {
