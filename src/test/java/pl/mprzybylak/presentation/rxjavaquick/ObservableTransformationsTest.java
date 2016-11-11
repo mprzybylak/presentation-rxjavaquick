6dbf3a7bff5f3b718f1c03b4fd3677f67ba7b203
Michał Przybylak <michal.przybylak@gmail.com>
2016-11-11 23:36:39 +0100

    concatMap example

--- src/test/java/pl/mprzybylak/presentation/rxjavaquick/ObservableTransformationsTest.java
+++ src/test/java/pl/mprzybylak/presentation/rxjavaquick/ObservableTransformationsTest.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ObservableTransformationsTest {
 
     @Test
@@ -23,11 +25,37 @@ public class ObservableTransformationsTest {
                 .subscribe(output::add);
 
         // then
-        Assertions.assertThat(output.get(0)).isEqualTo(input.get(0) * 2);
-        Assertions.assertThat(output.get(1)).isEqualTo(input.get(1) * 2);
-        Assertions.assertThat(output.get(2)).isEqualTo(input.get(2) * 2);
-        Assertions.assertThat(output.get(3)).isEqualTo(input.get(3) * 2);
-        Assertions.assertThat(output.get(4)).isEqualTo(input.get(4) * 2);
+        assertThat(output.get(0)).isEqualTo(input.get(0) * 2);
+        assertThat(output.get(1)).isEqualTo(input.get(1) * 2);
+        assertThat(output.get(2)).isEqualTo(input.get(2) * 2);
+        assertThat(output.get(3)).isEqualTo(input.get(3) * 2);
+        assertThat(output.get(4)).isEqualTo(input.get(4) * 2);
+    }
+
+    @Test
+    public void mapToObservableAndConcatWithOrdering() {
+
+        // given
+        List<Integer> input = Arrays.asList(1,2,3);
+        List<Integer> output = new ArrayList<>(15);
+
+        // when
+        Observable.fromIterable(input)
+                .concatMap(i -> Observable.range(i, 3))
+                .subscribe(output::add);
+
+        // then
+        assertThat(output.get(0)).isEqualTo(1);
+        assertThat(output.get(1)).isEqualTo(2);
+        assertThat(output.get(2)).isEqualTo(3);
+
+        assertThat(output.get(3)).isEqualTo(2);
+        assertThat(output.get(4)).isEqualTo(3);
+        assertThat(output.get(5)).isEqualTo(4);
+
+        assertThat(output.get(6)).isEqualTo(3);
+        assertThat(output.get(7)).isEqualTo(4);
+        assertThat(output.get(8)).isEqualTo(5);
     }
 
     @Test
@@ -39,16 +67,18 @@ public class ObservableTransformationsTest {
 
         // when
         Observable.fromIterable(input)
-                .cast(Integer.class)
+                .cast(Integer.class).flatMap(integer -> Observable.range(integer, 5))
                 .subscribe(output::add);
 
         // then
-        Assertions.assertThat(output.get(0)).isEqualTo(input.get(0));
-        Assertions.assertThat(output.get(1)).isEqualTo(input.get(1));
-        Assertions.assertThat(output.get(2)).isEqualTo(input.get(2));
-        Assertions.assertThat(output.get(3)).isEqualTo(input.get(3));
-        Assertions.assertThat(output.get(4)).isEqualTo(input.get(4));
+        assertThat(output.get(0)).isEqualTo(input.get(0));
+        assertThat(output.get(1)).isEqualTo(input.get(1));
+        assertThat(output.get(2)).isEqualTo(input.get(2));
+        assertThat(output.get(3)).isEqualTo(input.get(3));
+        assertThat(output.get(4)).isEqualTo(input.get(4));
     }
+
+
 
 
 }
