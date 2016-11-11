16ae6c38b3da3ef05495f0fc08be25aee97255c9
Michał Przybylak <michal.przybylak@gmail.com>
2016-11-11 22:50:44 +0100

    map exapmle

---
+++ src/test/java/pl/mprzybylak/presentation/rxjavaquick/ObservableTransformationsTest.java
@@ -0,0 +1,34 @@
+package pl.mprzybylak.presentation.rxjavaquick;
+
+import io.reactivex.Observable;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ObservableTransformationsTest {
+
+    @Test
+    public void simpleTransform() {
+
+        // given
+        List<Integer> input = Arrays.asList(1,2,3,4,5);
+        List<Integer> output = new ArrayList<>(5);
+
+        // when
+        Observable.fromIterable(input)
+                .map(i -> i * 2)
+                .subscribe(output::add);
+
+        // then
+        Assertions.assertThat(output.get(0)).isEqualTo(input.get(0) * 2);
+        Assertions.assertThat(output.get(1)).isEqualTo(input.get(1) * 2);
+        Assertions.assertThat(output.get(2)).isEqualTo(input.get(2) * 2);
+        Assertions.assertThat(output.get(3)).isEqualTo(input.get(3) * 2);
+        Assertions.assertThat(output.get(4)).isEqualTo(input.get(4) * 2);
+    }
+
+
+}
