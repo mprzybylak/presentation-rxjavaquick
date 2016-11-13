ef97319320ebaf5e2a0cb85a932a614600b6dd2b
Michał Przybylak <michal.przybylak@gmail.com>
2016-11-13 18:46:21 +0100

    operations in different threads

--- src/test/java/pl/mprzybylak/presentation/rxjavaquick/ObservableAndSchedulersTest.java
+++ src/test/java/pl/mprzybylak/presentation/rxjavaquick/ObservableAndSchedulersTest.java
@@ -5,6 +5,9 @@ import io.reactivex.schedulers.Schedulers;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -37,7 +40,7 @@ public class ObservableAndSchedulersTest {
     }
 
     @Test
-    public void observeOnDifferentThread() {
+    public void observeOnDifferentThread() throws InterruptedException {
 
         // given
         AtomicReference<Thread> defaultThread = new AtomicReference<>();
@@ -53,10 +56,82 @@ public class ObservableAndSchedulersTest {
                 .doOnEach(e -> switchedThread.set(Thread.currentThread()))
                 .subscribe();
 
+        Thread.sleep(100);
+
         // then
         assertThat(mainThread.get().getId()).isEqualTo(mainThread.get().getId());
         assertThat(switchedThread.get().getId()).isNotEqualTo(mainThread.get().getId());
     }
 
+    @Test
+    public void orderedSequenceOfNumbersWithGivenInterval() throws InterruptedException {
 
+        // given
+        CountDownLatch latch = new CountDownLatch(1);
+        List<Integer> sequence = new ArrayList<>(10);
+        LocalDateTime start = LocalDateTime.now();
+
+        // when
+        Observable.intervalRange(0, 100, 0, 10, TimeUnit.MILLISECONDS)
+                .subscribe(l -> {
+                }, t -> {
+                }, latch::countDown);
+
+        latch.await();
+
+        LocalDateTime end = LocalDateTime.now();
+
+        // then
+        int difference = (int)start.until(end, ChronoUnit.MILLIS);
+        assertThat(difference).isBetween(900, 1200);
+
+    }
+
+    @Test
+    public void interval() throws InterruptedException {
+
+        // given
+        List<Long> results = new ArrayList<>(10);
+
+        // when
+        Observable.interval(3, 1, TimeUnit.MILLISECONDS)
+                .take(10)
+                .subscribe(results::add);
+
+        Thread.sleep(1000);
+
+        // then
+        assertThat(results.get(0)).isEqualTo(0);
+        assertThat(results.get(1)).isEqualTo(1);
+        assertThat(results.get(2)).isEqualTo(2);
+        assertThat(results.get(3)).isEqualTo(3);
+        assertThat(results.get(4)).isEqualTo(4);
+        assertThat(results.get(5)).isEqualTo(5);
+        assertThat(results.get(6)).isEqualTo(6);
+        assertThat(results.get(7)).isEqualTo(7);
+        assertThat(results.get(8)).isEqualTo(8);
+        assertThat(results.get(9)).isEqualTo(9);
+    }
+
+    @Test
+    public void timer() throws InterruptedException {
+
+        // given
+        CountDownLatch latch = new CountDownLatch(1);
+        LocalDateTime start = LocalDateTime.now();
+        AtomicReference<LocalDateTime> ends = new AtomicReference<>();
+
+        // wait
+        Observable.timer(1, TimeUnit.SECONDS)
+                .subscribe(aLong -> ends.set(LocalDateTime.now()),
+                        throwable -> {
+                        },
+                        latch::countDown
+                );
+        latch.await();
+
+        // then
+        int difference = (int)start.until(ends.get(), ChronoUnit.SECONDS);
+        assertThat(difference).isBetween(1, 2);
+    }
 }
